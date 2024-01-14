87987bc87c9feef9514b30120cb2f30d85028c9a
gabang2 <offbeat1020@naver.com>
2024-01-15 01:03:53 +0900

    Feat : 속도 조절, 하단으로 바로가기

--- scrollbottom.js
+++ scrollbottom.js
@@ -1,3 +1,15 @@
 function scrollToTop() {
-  window.scrollTo({top: 5200, behavior: 'smooth'});
+    window.scrollTo({top: 5200, behavior: 'smooth'});
 }
+
+
+window.addEventListener('scroll', function () {
+    var fixedButton = document.getElementById('shortcut-button');
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (scrollTop > 4800) {
+        fixedButton.style.display = 'none';
+    } else {
+        fixedButton.style.display = 'inline-flex';
+    }
+});
